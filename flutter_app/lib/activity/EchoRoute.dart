d78b9662fb865264561b2154815cbf238a84addf
meiyingxiang <1048032351@qq.com>
2020-10-14 17:30:22 +0800

    update

--- flutter_app/lib/activity/EchoRoute.dart
+++ flutter_app/lib/activity/EchoRoute.dart
@@ -5,5 +5,13 @@ class EchoRoute extends StatelessWidget {
   Widget build(BuildContext context) {
     var args = ModalRoute.of(context).settings.arguments;
     print("传递过来的参数$args");
+
+    return new DecoratedBox(
+      decoration: new BoxDecoration(
+        image: new DecorationImage(
+          image: new AssetImage('assets/pi.png'),
+        ),
+      ),
+    );
   }
 }
